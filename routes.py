c4eaa6a9efdb9f8b1428a3bcf0f3e04d15b2ae3c
Ruby Wilkins <20563@burnside.school.nz>
2023-05-23 16:54:48 +1200

    added movements to the website

--- routes.py
+++ routes.py
@@ -7,11 +7,11 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-app.route("/about")
+@app.route("/about")
 def about():
     return render_template("about.html")
 
-app.route("/contact")
+@app.route("/contact")
 def contact():
     return render_template("contact.html")
 
