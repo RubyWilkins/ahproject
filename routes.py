4c772294a9b38827d5f2dffc46871025f12024c9
Ruby Wilkins <20563@burnside.school.nz>
2023-09-19 18:48:39 +1200

    added artist pages

--- routes.py
+++ routes.py
@@ -36,13 +36,15 @@ def movement(id):
     conn = sqlite3.connect("Art.db")
     cur = conn.cursor()
     # the id is specifying which movement
-    cur.execute("SELECT name,description FROM Movements WHERE id=?", (id,))
+    cur.execute("SELECT name, description FROM Movements WHERE id=?", (id,))
     mov = cur.fetchone()
+    # if the mid doesn't exist return 404
+    if not mov:
+        return render_template('404.html')
     # the mid is the movement id
     cur.execute("SELECT aid, name, image FROM Work WHERE mid=?", (id,))
     works = cur.fetchall()
-    print(mov)
-    print(works)
+    # if there were no works for this movement works will be [] which isn't an error
     return render_template("movement.html", name=mov[0], description=mov[1], results=works)
 
 
@@ -50,17 +52,45 @@ def movement(id):
 @app.route("/work/<int:id>")
 def work(id):
     conn = sqlite3.connect("Art.db")
-    cur = conn. cursor()
+    cur = conn.cursor()
     # the aid is the artwork's id
     cur.execute("SELECT name, description,image FROM Work WHERE aid=?", (id,))
     work = cur.fetchone()
-    print("Hello World", work)
+    # if the aid doesn't exist return 404
+    if not work:
+        return render_template('404.html')
     return render_template("work.html", name=work[0], description=work[1], item=work[2])
 
 
-# this is the code for the search 
+# this is the code for the artists
+@app.route("/artists")
+def artists():
+    conn = sqlite3.connect("Art.db")
+    cur = conn.cursor()
+    cur.execute("SELECT id, name FROM Artist")
+    results = cur.fetchall()
+    return render_template("all_artists.html", results=results)
 
 
+# this displays all of the artworks for a specific artist
+@app.route("/artist/<int:id>")
+def artist(id):
+    conn = sqlite3.connect("Art.db")
+    cur = conn.cursor()
+    # the id is specifying which artist
+    cur.execute("SELECT name, description FROM Artist WHERE id=?", (id,))
+    artist = cur.fetchone()
+    # if the aid doesn't exist return 404
+    if not artist:
+        return render_template('404.html')
+    # the rid is the artist id
+    cur.execute("SELECT aid, name, image FROM Work WHERE rid=?", (id,))
+    works = cur.fetchall()
+    # if there were no works for this artist works will be [] which isn't an error
+    return render_template("artist.html", name=artist[0], description=artist[1], results=works)
+
+
+# this is the code for the search
 @app.route("/search")
 def search():
     query = request.args.get('query', type=str)
@@ -72,16 +102,17 @@ def search():
     cur = conn.cursor()
 
     # Use a parameterized query to avoid SQL injection
-    cur.execute(f"SELECT aid, name FROM Work WHERE name LIKE ?", ('%' + query + '%',))
+    cur.execute("SELECT aid, name FROM Work WHERE name LIKE ?", ('%' + query + '%',))
     wresults = cur.fetchall()
 
-    cur.execute(f"SELECT aid, name FROM Work WHERE rid IN \
+    cur.execute("SELECT aid, name FROM Work WHERE rid IN \
                 (SELECT id FROM Artist WHERE name LIKE ?)",
                 ('%' + query + '%',))
     aresults = cur.fetchall()
     results = wresults + aresults
 
-    # there can be duplicates because a work can appear in wresults and aresults, so i added this to remove them
+    # there can be duplicates because a work can appear in wresults and
+    # aresults, so i added this to remove them
     results = list(set(results))
 
     cur.close()
